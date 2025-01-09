b3e6b26c0e7a418d0816538ee68faf670add5be6
king-Abby <iyiolaabby@gmail.com>
2025-01-09 11:57:20 +0100

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello World</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab officiis
+        voluptate corrupti in autem aliquam tempore nostrum quis iure cumque?
+      </p>
+
+      <p>Hello World</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab officiis
+        voluptate corrupti in autem aliquam tempore nostrum quis iure cumque?
+      </p>
+
+      <p>Hello World</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab officiis
+        voluptate corrupti in autem aliquam tempore nostrum quis iure cumque?
+      </p>
+    </>
+  );
+}
+
+export default App;
